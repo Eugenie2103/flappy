79366b1d6a9f1e3aa30f037d2b31e7e2e2fe9cba
GIGI <gigi@github.com>
2018-03-01 10:02:28 +0100

    hh

--- script.js
+++ script.js
@@ -1,57 +1,3 @@
-function saut() {
-  $("#flappy").css({marginBottom:'300px'})
-}
-
-function sauter(){
-  $("#flappy").animate({marginBottom: "+=100px"},speed=20,easing="swing");
-  $("#flappy").animate({marginBottom: "+=200px"},speed=20,easing="swing");
-  $("#flappy").animate({marginBottom: "+=-100px"},speed=20,easing="swing");
-}
-// $("#flappy").click(function(){
-// alert("Coucou")
-// });
-
-$("#flappy").click(function(touche){
-  console.log(touche);
-});
-
-$("#flappy").keydown(function(touche){
-  alert('tr');
-  console.log(touche);
-});
-
-
-
-//$( "#flappy" ).keydown(function() {
-  //alert( "Handler for .keydown() called." );
-//});
-
-function deplacement_vertical(sens){
-  if(sens == 1){
-    deplacement = "+=100px";
-  }
-  else{
-    deplacement = "-=100px";
-  }
-  $("#flappy").animate(
-    {
-      "marginTop": deplacement
-    },
-    500,
-    'swing'
-  )
-}
-
-
-$("img").ready( function () {
-$("img").on('click',function () {
-  alert("FLAPPY EST DANS L'ESPACE !!!");
-    });
-});
-
-
-
-
 function bougerHaut(){
 $("#flappy").animate({marginBottom:'+=100px'}, speed=200, easing="swing");
      }
@@ -101,20 +47,4 @@ $(document).keyup(function(touche){
             if(appui == 39){
                 bougerDroite();
            }
-});
-
-$("#flappy").click (function (touche) {
-alert("Coucou");
-console.log(touche);
-});
-
-$("#flappy").click(function (touche) {
-   console.log(touche.type);
-});
-
-
-$(document).keydown(function (bra) {
-if (bra.keyCode == 65){
-alert("A");
-}
 });
